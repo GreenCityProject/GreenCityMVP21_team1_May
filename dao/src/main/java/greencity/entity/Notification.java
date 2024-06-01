670c794fbb9c961afdb8ed64dce665e195dddaef
Olexandr Mozil <sashamozil@gmail.com>
2024-06-01 04:33:57 +0300

    [224] add Notification entity and Dto

---
+++ dao/src/main/java/greencity/entity/Notification.java
@@ -0,0 +1,53 @@
+package greencity.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "notifications")
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_user_id")
+    private User senderUser;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "section", nullable = false)
+    private String section;
+
+    @Size(max = 255)
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @Column(name = "message", nullable = false, length = Integer.MAX_VALUE)
+    private String message;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd-HH-mm-ss.zzz")
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false;
+
+}
