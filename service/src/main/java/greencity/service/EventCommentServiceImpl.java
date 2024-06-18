84a3926e8d453745725cf6e0ee5c39bc76885ce1
df7734 <df7734@gmail.com>
2024-06-18 15:09:44 +0300

    [171] minor changes

--- service/src/main/java/greencity/service/EventCommentServiceImpl.java
+++ service/src/main/java/greencity/service/EventCommentServiceImpl.java
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
